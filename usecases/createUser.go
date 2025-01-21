a32403fc730b739ac1136da7db3fe5438becb258
midnigh8k <midnigh8k@sapo.pt>
2025-01-21 22:04:22 +0000

    Add documentation and README.md

--- usecases/createUser.go
+++ usecases/createUser.go
@@ -5,14 +5,19 @@ import (
 	"blankproject/repository"
 )
 
+// Interface for user Service.
 type UserService interface {
 	SaveUser(user *entities.User) error
 }
 
+// Instantiation of REPO property that will be used to access to the repository of users.
 type RepoUserUseCase struct {
 	REPO repository.UserRepository
 }
 
+// This is the usecase SaveUser.
+// This function receives the entity object User, that contains all properties of the user.
+// Return: save the entity User in database.
 func (s *RepoUserUseCase) SaveUser(user *entities.User) error {
 	return s.REPO.Save(user)
 }
